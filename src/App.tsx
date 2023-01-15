2b712fe864e3f54837799d954fd08c723181e79d
wisholli <osribna4@gmail.com>
2023-01-15 15:15:11 +0200

    start

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Container } from "components/Container";
+import { Search } from "components/Search";
+import { TheHeader } from "components/TheHeader";
+
+function App() {
+  return (
+    <Container>
+      <TheHeader />
+      <Search onSubmit={() => {}} />
+    </Container>
+  );
+}
+
+export default App;
